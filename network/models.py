8174ae12aebbbb2c4c2ebd2fdb54583e6c2e4914
Allen <allenvs@mail.ru>
2021-04-19 22:09:58 +0300

    add author field to Post model

--- network/models.py
+++ network/models.py
@@ -3,16 +3,15 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
-    # following = models.ManyToManyField(User, blank=true)
-    # def __str__(self):
-    #     return f"User {self.name}"
-    
-# posts: posts of that user 
+    # pass
+    following = models.ManyToManyField(User, blank=true)
+    def __str__(self):
+        return f"User {self.name}"
 
-# class Post(models.Model):
-#     text = models.CharField(max_length=100)
-#     likes_count = models.PositiveIntegerField()
-#     likes_users = models.ManyToManyField(User, blank=True)
-#     def __str__(self):
-#         return f"{self.text}"
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=100)
+    likes_count = models.PositiveIntegerField() # stores the number of likes for this post
+    likes_users = models.ManyToManyField(User, blank=True) # this table stores pairs User:Post 
+    def __str__(self):
+        return f"{self.text}"
