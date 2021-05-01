43ca3019f160c263f905fe8a1e64fbb0cfb53349
Allen <allenvs@mail.ru>
2021-05-01 22:09:44 +0300

    question about promises

--- network/static/network/app.js
+++ network/static/network/app.js
@@ -4,7 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function onClick (){
         console.log("submit button pushed")
-        // here we will send form input to the server via an ajax POST request, refer to project Pizza  
+        // here we will send form input to the server via an ajax POST request, refer to project Pizza
+        // is there value in using promises to send post text to the server?  
     }
     document.querySelector('input').onclick = onClick;
     });
