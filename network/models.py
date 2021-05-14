7d2a48deddc87f1056e126d44beab70cca00dd08
Allen <allenvs@mail.ru>
2021-05-14 21:59:39 +0300

    added related names -followers and -posts

--- network/models.py
+++ network/models.py
@@ -4,12 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     # pass
-    following = models.ManyToManyField('self', blank=True) # how to avoid following yourself
+    following = models.ManyToManyField('self', blank=True, related_name="followers") # how to avoid following yourself
     def __str__(self):
         return f"{self.name}"
 
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="authors") # authors? really?
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts") # authors? really?
     text = models.CharField(max_length=100)
     likes_count = models.PositiveIntegerField() # stores the number of likes for this post
     likes_users = models.ManyToManyField(User, blank=True) # this table stores pairs User:Post
