8f051e0ed1aafca3ccc05ecc311dba3a2d502b35
Allen <allenvs@mail.ru>
2021-07-09 21:43:11 +0300

    removed dataset from post text line in html

--- network/static/network/edit_button.js
+++ network/static/network/edit_button.js
@@ -30,7 +30,7 @@ class EditButton extends React.Component {
                 this.setState({ editing: true });
                 const post = document.getElementById(this.props.postID);
                 console.log(post.innerText);
-                document.getElementById(this.props.postID).innerHTML ='<textarea maxlength="500" cols="50" rows="2">'+post.dataset.posttext+'</textarea>'
+                document.getElementById(this.props.postID).innerHTML ='<textarea maxlength="500" cols="50" rows="2">'+post.innerText+'</textarea>'
             }
         },
       'Edit ' + this.props.postID
