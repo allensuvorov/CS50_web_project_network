85074f135cac0ab5776cbe18c50fd7ca83e302f3
Allen <allenvs@mail.ru>
2021-07-21 21:39:06 +0300

    models.py - clean

--- network/models.py
+++ network/models.py
@@ -3,17 +3,21 @@ from django.db import models
 
 
 class User(AbstractUser):
-    # pass
-    following = models.ManyToManyField('self', symmetrical=False, related_name="followers", blank=True) # how to avoid following yourself
+    following = models.ManyToManyField('self', symmetrical=False, related_name="followers", blank=True)
     def __str__(self):
         return f"{self.username}"
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     text = models.CharField(max_length=100)
-    likes_count = models.PositiveIntegerField(default=0) # stores the number of likes for this post
-    likes_users = models.ManyToManyField(User, blank=True) # this table stores pairs User:Post
+    
+    # stores the number of likes for this post
+    likes_count = models.PositiveIntegerField(default=0) 
+    
+    # this table stores pairs Post:User
+    likes_users = models.ManyToManyField(User, blank=True) 
+    
+    # the date and time at which the post was created
     date_time = models.DateTimeField(auto_now=False, auto_now_add=True) 
-    # the date and time at which the post was made
     def __str__(self):
         return f"USER: {self.author}, POST: {self.text}, {self.date_time.strftime('%d-%m-%Y %H:%M:%S')}, LIKES: {self.likes_count}"
